bd049f8a74b7501e35dd89ae9e019da3e178025d
Hannah huggins <dowdallhannah@yahoo.com>
2019-02-07 11:03:34 +0000

    Fixing github

--- presentation-services/script.js
+++ presentation-services/script.js
@@ -1,3 +1,18 @@
-var json = require('testjson.json');
-console.log(JSON);
+var jsonVar = { 
+    businessName: "Test Business", 
+    postCode: "P05T CD3", 
+    ratingValue: "Awaiting Inspection", 
+}
 
+var jsonStr = JSON.stringify(jsonVar);
+
+document.getElementById("test-para").innerHTML = displayData(jsonVar);
+
+function displayData(business){
+    var output = "";
+    output += business.businessName + "<br />";
+    output += business.postCode + "<br />";
+    output += business.ratingValue + "<br />";
+
+    return output;
+}
